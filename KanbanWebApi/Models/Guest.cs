595fcfb81846486aa7c05ed1e11bbcf071a35791
Eric <eric.bisbjerg@hotmail.com>
2023-05-02 18:35:32 +0200

    Models and efcore

---
+++ KanbanWebApi/Models/Guest.cs
@@ -0,0 +1,18 @@
+﻿namespace KanbanWebApi.Models
+{
+    public class Guest
+    {
+        public int Id { get; set; }
+        public bool CanComplete { get; set; }
+        public bool CanCreate { get; set; }
+        public bool CanAssign { get; set; }
+
+        // FK
+        public int KanbanBoardId { get; set; }
+        public int UserId { get; set; }
+
+        // FK Relation prop
+        public KanbanBoard KanbanBoard { get; set; }
+        public User User { get; set; }
+    }
+}
---
+++ KanbanWebApi/Models/KanbanBoard.cs
@@ -0,0 +1,15 @@
+﻿namespace KanbanWebApi.Models
+{
+    public class KanbanBoard
+    {
+        public int Id { get; set; }
+        public string Titel { get; set; }
+        public DateTime DeadLine { get; set; }
+
+        // Foreign Key
+        public int UserId { get; set; }
+
+        // FK Relation prop
+        public User User { get; set; }
+    }
+}

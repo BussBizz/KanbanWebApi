edda3a718caf35f8f302a5e93f66cbe2ab1c3b8a
Eric <eric.bisbjerg@hotmail.com>
2023-05-05 13:28:27 +0200

    Startup and appsettings

---
+++ KanbanWebApi/Startup.cs
@@ -0,0 +1,52 @@
+﻿using KanbanWebApi.DB;
+using Microsoft.EntityFrameworkCore;
+
+namespace KanbanWebApi
+{
+    public class Startup
+    {
+        public IConfiguration Configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            services.AddDbContext<KanbanDBContext>(opt => opt
+                .UseSqlServer(Configuration.GetConnectionString("KanbanConnectionString"))
+            );
+
+            services.AddEndpointsApiExplorer();
+
+            services.AddSwaggerGen();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseHttpsRedirection();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
